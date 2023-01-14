3145b0f7ec66e8f8ff145b0da2979c5acf37afc5
Claiment <36105328+Heliooz@users.noreply.github.com>
2023-01-14 19:10:45 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,6 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+            <h1> App of Clément Maurer</h1>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
